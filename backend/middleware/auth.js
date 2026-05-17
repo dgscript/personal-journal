77f003dd3165bee83c210be174e29a5509fc331a
rezird <148511997+rezird@users.noreply.github.com>
2026-05-16 23:03:48 -0300

    fix: auth issues

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,17 +5,15 @@ const JWT_PASSWORD = process.env.JWT_PASSWORD;
 
 export function authMiddleware(req, res, next) {
   try {
-    const auth = req.cookies;
+    const token = req.cookies.token;
 
-    if (!auth || auth.length === 0)
+    if (!token)
       return res
         .status(401)
-        .json({ message: "Authorization token needed to peform this action." });
+        .json({
+          message: "Authorization token needed to perform this action.",
+        });
 
-    /* if (!auth.startsWith("Bearer "))
-      return res.status(401).json({ message: "Invalid token format." }); */
-
-    const token = auth.token;
     const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
 
     req.user = decoded;
