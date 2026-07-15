1ac0f5f390787adce507098eb51c82c55cdcd093
rezird <148511997+rezird@users.noreply.github.com>
2026-07-15 14:21:38 -0300

    update: new username

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,10 @@ import "../styles/footer.css";
 export default function Footer() {
   return (
     <footer>
-      Personal Journal — Made by <span>dgscript</span>
+      Personal Journal — Made by{" "}
+      <a href="https://github.com/dgsqr" target="_blank">
+        dgsqr
+      </a>
     </footer>
   );
 }
